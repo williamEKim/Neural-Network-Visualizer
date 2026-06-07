7efa1aeadf0e6e31b3e3382f8289dc26eaafc432
williamEkim <dongyeon0308@gmail.com>
2026-06-07 04:41:47 -0700

    updated sigmoid, sigmoid prime, and forward pass algo

--- model/train.py
+++ model/train.py
@@ -5,11 +5,13 @@ import torchvision.transforms as transforms
 
 # --- Activation functions ---
 def sigmoid(z):
-    pass  # TODO
+    # σ(z) = 1 / (1 + e^-z)
+    return 1 / (1 + np.e ** (-z))
 
 def sigmoid_prime(z):
     # derivative of sigmoid — you'll need this for backprop
-    pass  # TODO
+    # σ'(z) = σ(z) * [1 - σ(z)]
+    return  sigmoid(z) * (1 - sigmoid(z))
 
 # --- Forward pass ---
 def forward(x, weights, biases):
@@ -17,4 +19,14 @@ def forward(x, weights, biases):
     # weights is a list of weight matrices, one per layer
     # biases is a list of bias vectors, one per layer
     # return: list of activations for EACH layer (you'll need these for backprop)
-    pass  # TODO
+    activation_list = []
+    a = x
+
+    # add a initial activation layer to a list
+    activation_list.append(a)
+
+    for index, weight in enumerate(weights):
+        a = sigmoid(weight @ a + biases[index])
+        activation_list.append(a)
+
+    return activation_list
